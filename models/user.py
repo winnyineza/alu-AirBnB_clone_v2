ae408bb57b13336dd300ba9eb8da8cd8a199e641
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2023-10-01 09:22:49 +0200

    user.py

--- models/user.py
+++ models/user.py
@@ -9,19 +9,23 @@ from models.review import Review
 
 
 class User(BaseModel, Base):
-    """This is the class for user
+    """Represents a user for a MySQL database.
+
+    Inherits from SQLAlchemy Base and links to the MySQL table users.
+
     Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
+        __tablename__ (str): The name of the MySQL table to store users.
+        email: (sqlalchemy String): The user's email address.
+        password (sqlalchemy String): The user's password.
+        first_name (sqlalchemy String): The user's first name.
+        last_name (sqlalchemy String): The user's last name.
+        places (sqlalchemy relationship): The User-Place relationship.
+        reviews (sqlalchemy relationship): The User-Review relationship.
     """
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="user")
-    reviews = relationship("Review", cascade='all, delete, delete-orphan',
-                           backref="user")
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
