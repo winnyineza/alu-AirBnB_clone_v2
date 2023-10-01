5f64b2da901deda81d3be5d5810ed8198181de87
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2023-10-01 09:55:18 +0200

    state.py

--- models/state.py
+++ models/state.py
@@ -8,12 +8,11 @@ from models.base_model import BaseModel, Base
 class State(BaseModel, Base):
     """This is the class for State
     Attributes:
-        name: input name
+        name:input name
     """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    
-    # Define the one-to-many relationship with City
+ # Define the one-to-many relationship with City
     cities = relationship("City", cascade='all, delete-orphan', backref="state")
 
     def __init__(self, *args, **kwargs):
